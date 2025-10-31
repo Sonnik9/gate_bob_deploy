d26eb475696dba7a9ae9d6628bf3a3d5c7333a5e
kali <kali@kali.netis.cc>
2025-10-31 17:11:11 +0200

    ftry to deploy by mi

--- a_config.py
+++ a_config.py
@@ -5,8 +5,8 @@ TEG_ANCHOR_SET: Set = {"#soft", "trading pair", } # --------------------------  
 
 # --- SECRETS CONFIG ---                 
 # TG_BOT_TOKEN: str = "8190920390:AAE09pWhSVguG0iiBNIztM2Pe8zECPV4vSg" # токен тг бота old fin klient
-TG_BOT_TOKEN: str = "7950631691:AAFntHbnAJlKcaTkypXTw2lVPWagiz3b_ak" # токен тг бота2 old fin klient
-# TG_BOT_TOKEN: str = "8344277656:AAE6BdsqUY7So0oO2iX8B3e0G2w6FHw8D8E" # 
+# TG_BOT_TOKEN: str = "7950631691:AAFntHbnAJlKcaTkypXTw2lVPWagiz3b_ak" # токен тг бота2 old fin klient
+TG_BOT_TOKEN: str = "8344277656:AAE6BdsqUY7So0oO2iX8B3e0G2w6FHw8D8E" # 
 
 # -- UTILS ---
 # BLACK_SYMBOLS: set = {"BTC_USDT"} # -------------# символы-исключения (не используем в торговле)
@@ -41,8 +41,10 @@ EMO_ORDER_FILLED: str = "🤞"
 INIT_USER_CONFIG = {
     "config": {
         "GATE": {
-            "api_key": "",
-            "api_secret": "",
+            # "api_key": "",
+            # "api_secret": "",
+            "api_key": "925d3d629038c1c57655a5dac692911d",
+            "api_secret": "d63ed01fca8cfb1a28507de3a96617e2362591725a355315fec060b3595022e3",
         },
         "fin_settings": {
             "trading pair": {
@@ -50,7 +52,7 @@ INIT_USER_CONFIG = {
                 "margin_mode": 2, # 1 - Изолиркаб 2 -- CROSSED
                 "trigger_order_type": 2, # Тип тригерного ордера -- 1 лимитный, 2 рыночный  
                 "order_type": 2, # 1 Тип ордера -- лимитками, 2 -- по маркету. 
-                "leverage": 0, #  плечо. 0 -- будет брать из сигналов
+                "leverage": 15, #  плечо. 0 -- будет брать из сигналов
                 "dop_tp": None, # дополнительный тейк профит в % 1 - 100          
                 "order_timeout": 60, # таймаут лимитного ордера в секундах
             },
@@ -59,7 +61,7 @@ INIT_USER_CONFIG = {
                 "margin_mode": 2, # 1 - Изолиркаб 2 -- CROSSED
                 "trigger_order_type": 2, # Тип тригерного ордера -- 1 лимитный, 2 рыночный  
                 "order_type": 2, # 1 Тип ордера -- лимитками, 2 -- по маркету. 
-                "leverage": 0, #  плечо. 0 -- будет брать из сигналов
+                "leverage": 15, #  плечо. 0 -- будет брать из сигналов
                 "dop_tp": None, # дополнительный тейк профит в % 1 - 100          
                 "order_timeout": 60, # таймаут лимитного ордера в секундах
             }
